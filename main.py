a5cd60838bfada22b1ca03897cedd4bfedc51cc4
somberlyx <lybasiddiqui02@gmail.com>
2024-08-26 18:54:29 +0400

    Find out the number of male and female students

--- main.py
+++ main.py
@@ -15,4 +15,13 @@ df = pd.read_csv("studentResults.csv")
 df = df.drop("Unnamed: 0", axis = 1)
 print(df.head())
 
-df["WklyStudyHours"] = df["WklyStudyHours"].str.replace("05-Oct", "05-10")
+df["WklyStudyHours"] = df["WklyStudyHours"].str.replace("05-Oct", "05-10") 
+
+#Gender Ditribution
+plt.figure(figsize=(5,5))
+ax = sns.countplot(data = df, x = 'Gender')
+ax.bar_label(ax.containers[0])
+plt.show()
+
+
+
