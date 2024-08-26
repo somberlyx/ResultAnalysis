69d3c6d697cd3c8ddcedcd813273844bc6af4089
somberlyx <lybasiddiqui02@gmail.com>
2024-08-26 18:44:07 +0400

    Make data more readable by removing different format

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import seaborn as sns
 
 df = pd.read_csv("studentResults.csv")
 
-print(df.describe())
+# print(df.describe())
 
-print(df.info())
+# print(df.info())
 
-print(df.isnull().sum())
+# print(df.isnull().sum())
 
 #Remove Unnamed Column
 df = df.drop("Unnamed: 0", axis = 1)
 print(df.head())
+
+df["WklyStudyHours"] = df["WklyStudyHours"].str.replace("05-Oct", "05-10")
